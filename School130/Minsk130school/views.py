2d48410cee21d7b2b55f3b5b3302d9bcfd7d20e5
VictoriaSavanchuke <vikula-zah@mail.ru>
2023-11-14 20:41:03 +0300

    оформление главной стр

--- School130/Minsk130school/views.py
+++ School130/Minsk130school/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
+
+def home (request):
+    return render(request, 'base.html', {'home':home})
+
+def slider_view(request):
+    courses = models.PaidServices.objects.all()
+    return render(request, 'base.html', {'courses': courses})
